240e7df671f5eeee6fa61a8824ea7c4b7c25c160
npinto97 <ningo1797@gmail.com>
2022-07-11 15:13:01 +0200

    Creazione classe GameScreenManager

---
+++ src/main/java/GUI/GameScreenManager.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GameScreenManager
+{
+    private final JLayeredPane gameScreenPanel;
+
+    public GameScreenManager(JLayeredPane gameScreenPanel)
+    {
+        this.gameScreenPanel = gameScreenPanel;
+    }
+}
